4765f5671707126fd9381f1f076399dc77416339
JSJ <jsj950611@naver.com>
2024-01-10 21:07:40 +0900

    start

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.set()
+app.set('view engine', 'ejs')
+app.set(express.urlencoded({extended : false}))
+
+app.use('/', function(req,res){
+    res.render('main.ejs')
+})
