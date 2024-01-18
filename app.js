7cc491270bc9b9b5860f2641396d91e146a3f9c2
JSJ <jsj950611@naver.com>
2024-01-18 21:45:20 +0900

    진짜 마지막

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require('path')
 const routers = require('./routers/router')
 
-app.set("views", __dirname+'/views');
+app.set("views", path.join(__dirname,'/views'));
 app.set("view engine", "ejs");
 app.set(express.urlencoded({ extended: false }));
-// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use(routers);
 app.listen(8000);
